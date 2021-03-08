ab92091c24d8063378181daae827410097127042
mtoutside <more.humor.or@gmail.com>
2021-03-08 16:57:35 +0900

    wip オブジェクトから座標取得途中

--- src/kvlt10/Canvas/_index.js
+++ src/kvlt10/Canvas/_index.js
@@ -1,72 +1,31 @@
 'use strict';
 
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+// import fragmentShader from './shader/frag.glsl';
+// import vertexShader from './shader/vert.glsl';
 import Config from './_Config';
-import fragmentShader from './shader/frag.glsl';
-import fragmentShader2 from './shader/frag2.glsl';
-import vertexShader from './shader/vert.glsl';
 
-const info = [
-  {
-    name: 'Rhino',
-    text:
-      'Rhinoceroses are large, herbivorous mammals identified by their characteristic horned snouts. The word "rhinoceros" comes from the Greek "rhino" (nose) and "ceros" (horn). ',
-  },
-  {
-    name: 'Lion',
-    text:
-      'Lions go on the hunt for food mostly from dusk till dawn. Female lions do 85-90% of the prides hunting, whilst the male lions patrol the territory and protect the pride.',
-  },
-  {
-    name: 'Leopard',
-    text:
-      'Leopards are skilled climbers, and like to rest in the branches of trees during the day. They are strong beasts, too, and can carry their heavy prey up into the trees so that pesky scavengers, such as hyenas, don’t steal their meal!',
-  },
-  {
-    name: 'Elephant',
-    text:
-      'These magnificent mammals spend between 12 to 18 hours eating grass, plants and fruit every single day! They use their long trunks to smell their food and lift it up into their mouth – yum!',
-  },
-  {
-    name: 'Giraffe',
-    text:
-      'Female giraffes give birth standing up. The result? Newborns are welcomed to the world with a 1.5m drop to the ground! Ouch! But these infants are quick to get on their feet – within 30 minutes they are standing, and only hours later they”re able to run with their mothers.',
-  },
-];
-
-const config = {
-  font: 'Josefin Sans',
-  size: 16,
-  h1size: 30,
-  padding: 10,
-  colour: '#fff',
-  width: 512,
-  height: 256,
-  zpos: 0.005,
-  planesize: { width: 1, height: 0.5 },
-};
-
-const uniforms2 = {
-  u_time: { value: 0.0 },
-  u_duration: { value: 2.0 },
-  u_twirls: { value: 7 },
-};
-
-const material2 = new THREE.ShaderMaterial({
-  uniforms: uniforms2,
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader2,
-  transparent: true,
-});
-
-const canvasText = new CanvasText(scene, info[1], config, material2);
-
-export default class {
+export default class Canvas {
   constructor() {
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
     this.clock = new THREE.Clock();
     this.container = document.getElementById('CanvasContainer');
+
+    this.particleIndexArray = [];
+    this.uniforms = {
+      time: {
+        type: 'f',
+        value: 0.0,
+      },
+      size: {
+        type: 'f',
+        value: 10.0,
+      },
+    };
+
     this.setConfig();
 
     this.renderer = new THREE.WebGLRenderer({
@@ -75,6 +34,7 @@ export default class {
     });
     this.renderer.setSize(Config.width, Config.height);
     this.renderer.setPixelRatio(Config.dpr);
+    this.renderer.setClearColor(0x98339a, 1);
     this.container.appendChild(this.renderer.domElement);
 
     this.resizeFunction = this.resize.bind(this);
@@ -91,10 +51,15 @@ export default class {
       1,
       10000
     );
-    this.camera.position.set(0, 0, 90);
-    this.scene.background = new THREE.Color(0x000000);
-    this.camera.lookAt(0, 0, 0);
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.camera.position.set(0, 0, 100);
+    this.directLight = new THREE.DirectionalLight(0x888888, 1);
+    this.ambLight = new THREE.AmbientLight(0xffffff, 0.65);
+    this.directLight.position.set(1, 1, 1).normalize();
+    this.scene.add(this.directLight);
+    this.scene.add(this.ambLight);
+    this.z = Math.min(window.innerWidth, window.innerHeight);
+    this.camera.lookAt(0, 0, this.z);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     // 初期化
     this.init();
@@ -119,31 +84,113 @@ export default class {
   }
 
   init() {
+
     this.start();
   }
 
-  initMesh() {
-    this.geometry = new THREE.PlaneGeometry(2, 1.5);
-    this.uniforms = {
-      u_tex_1: { value: null },
-      u_tex_2: { value: null },
-      u_duration: { value: 2.0 },
-      u_time: { value: 0.0 },
-      u_mouse: { value: { x: 0.0, y: 0.0 } },
-      u_resolution: { value: { x: 0, y: 0 } },
-    };
+  initObject() {
+    const objLoader = new OBJLoader();
+    const vertices = [];
+    this.objGeometory = new THREE.BufferGeometry();
+    let objData;
+    objLoader.load('./assets/images/skull.obj', (root) => {
+      if(root) {
+        root.rotation.x = 30;
+        root.rotation.z = 0;
+        this.scene.add(root);
+        objData = root.children[3].geometry.attributes.position.array;
+        for(let i = 0; i < 100; i++) {
+          console.log(`XYZ ${i}: ${objData[i]}, ${objData[i + 1]}, ${objData[i + 2]}`);
+        }
+      }
+    });
+  }
 
+  createParticles() {
+    const imageData = this.getImageData(this.video);
+    this.geometry = new THREE.BufferGeometry();
+    this.geometry.morphAttributes = {};
     this.material = new THREE.ShaderMaterial({
-      uniforms: uniforms,
+      uniforms: this.uniforms,
       vertexShader: vertexShader,
       fragmentShader: fragmentShader,
+      transparent: true,
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
     });
 
-    this.plane = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.plane);
+    const vertices = [];
+    const colors = [];
+
+    let colorsPerFace = ['#ff4b78', '#16e36d', '#162cf8', '#2016e3'];
+
+    const step = 3;
+
+    // 格子状にパーティクルを並べる
+    for (let y = 0, height = imageData.height; y < height; y += step) {
+      for (let x = 0, width = imageData.width; x < width; x += step) {
+        let index = (x + y * width) * 4;
+        this.particleIndexArray.push(index);
+
+        let gray =
+          (imageData.data[index] +
+            imageData.data[index + 1] +
+            imageData.data[index + 2]) /
+          3;
+        let z = gray < 300 ? gray : 10000;
+        vertices.push(x - imageData.width / 2, -y + imageData.height / 2, z);
+
+        const rgbColor = this.hexToRgb(
+          colorsPerFace[Math.floor(Math.random() * colorsPerFace.length)]
+        );
+        colors.push(rgbColor.r, rgbColor.g, rgbColor.b);
+      }
+    }
+
+    const verticesArray = new Float32Array(vertices);
+    this.geometry.addAttribute(
+      'position',
+      new THREE.BufferAttribute(verticesArray, 3)
+    );
+
+    const colorsArray = new Float32Array(colors);
+    this.geometry.addAttribute(
+      'color',
+      new THREE.BufferAttribute(colorsArray, 3)
+    );
+
+    this.particles = new THREE.Points(this.geometry, this.material);
+    this.scene.add(this.particles);
+  }
+
+  getImageData(image, useCache) {
+    if (useCache && this.imageCache) {
+      return this.imageCache;
+    }
+
+    const w = image.videoWidth;
+    const h = image.videoHeight;
+
+    this.canvas.width = w;
+    this.canvas.height = h;
+
+    this.ctx.translate(w, 0);
+    this.ctx.scale(-1, 1);
+    this.ctx.drawImage(image, 0, 0);
+    this.imageCache = this.ctx.getImageData(0, 0, w, h);
+
+    return this.imageCache;
+  }
+
+  mesh() {
+    this.geometry = new THREE.BoxGeometry(10, 10, 10);
+    this.material = new THREE.MeshNormalMaterial();
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
   }
 
   start() {
+    this.initObject();
     this.resize();
     this.update();
   }
@@ -155,7 +202,51 @@ export default class {
 
   update() {
     this.clock.getDelta();
+    const t = this.clock.elapsedTime;
 
+    this.uniforms.time.value += 0.5;
+    let r, g, b;
+
+    // video
+    /*
+    if (this.particles) {
+      const useCache = parseInt(t) % 2 === 0;
+      const imageData = this.getImageData(this.video, useCache);
+      let count = 0;
+
+      for (
+        let i = 0,
+          length = this.particles.geometry.attributes.position.array.length;
+        i < length;
+        i += 3
+      ) {
+        let index = this.particleIndexArray[count];
+        let gray =
+          (imageData.data[index] +
+            imageData.data[index + 1] +
+            imageData.data[index + 2]) /
+          3;
+        let threshold = 300;
+        if (gray < threshold) {
+          if (gray < threshold / 3) {
+            this.particles.geometry.attributes.position.array[i + 2] =
+              gray * r * 5;
+          } else if (gray < threshold / 2) {
+            this.particles.geometry.attributes.position.array[i + 2] =
+              gray * g * 5;
+          } else {
+            this.particles.geometry.attributes.position.array[i + 2] =
+              gray * b * 5;
+          }
+        } else {
+          this.particles.geometry.attributes.position.array[i + 2] = 10000;
+        }
+        count++;
+      }
+      this.uniforms.size.value = ((r + g + b) / 3) * 35 + 5;
+      this.particles.geometry.attributes.position.needsUpdate = true;
+    }
+    */
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.updateFunction);
   }
